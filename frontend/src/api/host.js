11d06963321821c81e761bf51f2c7cef5bb80291
roma-cheby <105049918+roma-cheby@users.noreply.github.com>
2024-09-01 20:34:37 +0500

    change host

--- frontend/src/api/host.js
+++ frontend/src/api/host.js
@@ -2,6 +2,6 @@ let HOST;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
     HOST = 'http://localhost:5000/api';
 else
-    HOST = 'https://pincode-dev.ru/pp-manager/api';
+    HOST = 'https://dev.pincode-dev.ru/pp-manager/api';
 
 export default HOST;
